5af7efe886a793f2f74f2a4f848ad7657e715bd9
Chris Cornutt <chris.cornutt@hp.com>
2014-12-19 14:17:53 -0600

    adding in some styling

--- index.php
+++ index.php
@@ -17,11 +17,45 @@
             // Make the board and kick off the game
             var b = new Board(board, snake, draw);
             b.render();
+
+            $(document).on('click', '#board', function() {
+                b.reset();
+                $('#score-value').html(0);
+            });
         });
         </script>
+        <style>
+        body {
+            margin: 0px;
+            padding: 0px;
+            font-family: verdana, arial, helvetica;
+        }
+        #score {
+            background-color: #EEEEEE;
+            padding: 5px;
+        }
+        #score-value {
+            font-weight: bold;
+        }
+        #content {
+            width: 400px;
+            height: 90%;
+            text-align: center;
+            border: 1px solid #CCCCCC;
+            background-color: #8E7D85;
+            padding-top: 10px;
+        }
+        #board {
+            background-color: #FFFFFF;
+        }
+        </style>
     </head>
     <body>
-        <canvas id="board" width="110" height="110"></canvas>
-        <div id="score"></div>
+        <div id="content">
+            <h2 style="color:#FFFFFF">Let's Play Snake!</h2>
+            <canvas id="board" width="229" height="229"></canvas>
+            <br/><br/>
+            <div id="score">Score: <span id="score-value">0</span></div>
+        </div>
     </body>
 </html>
